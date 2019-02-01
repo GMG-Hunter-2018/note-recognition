5ef06e9d2fb0e975e2e747ef2b48711c1536dd64
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-02-01 14:42:19 -0500

    Rename lib -> libs to conform with pitchfork

--- lib/noter/hello.cpp
+++
@@ -1,7 +0,0 @@
-#include "noter/hello.hpp"
-
-using namespace std::string_literals;
-
-std::string hello() {
-	return "hello"s;
-}
---
+++ libs/noter/hello.cpp
@@ -0,0 +1,7 @@
+#include "noter/hello.hpp"
+
+using namespace std::string_literals;
+
+std::string hello() {
+	return "hello"s;
+}
