8fca36f12483b53eefcc2ab84ec55c7816bb3314
Eve Ali <evelynali99@gmail.com>
2019-02-01 01:11:28 -0500

    Removed local build files

--- build/tools/format.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-`type -p clang-format{-8,-7,-6.0,} | head -n 1` -i `find "/home/eve/Sync/Non-school-programs/GoogleNotes/note-recognition"/{src,lib} -type f -iname '*.cpp' -or -iname '*.h'`
--- build/tools/lint-clang.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-`type -p clang-tidy{-8,-7,-6.0,} | head -n 1` `find "/home/eve/Sync/Non-school-programs/GoogleNotes/note-recognition"/{lib,src} -type f -iname '*.cpp'` -p "/home/eve/Sync/Non-school-programs/GoogleNotes/note-recognition/build" "-header-filter=`echo '/home/eve/Sync/Non-school-programs/GoogleNotes/note-recognition/src/(../include|../lib|../src)/.*' | sed 's/\\//\\\\\//g'`"
